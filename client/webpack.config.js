75c24d5939cd23efa194ccc78e7843041871ad20
alexander rodriguez <alexander.rodriguez2121@gmail.com>
2024-08-25 15:06:32 -0600

    updated webpack.config.js

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -18,12 +18,68 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'JATE',
+      }),
+
+      // Generates the service worker file
+      new GenerateSW({
+        clientsClaim: true,
+        skipWaiting: true,
+      }),
+
+      // Injects manifest
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js',
+      }),
+
+      // Generates the manifest file 
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: 'JATE - Just Another Text Editor',
+        short_name: 'JATE',
+        description: 'Create notes or code snippets with or without an internet connection so that a user can reliably retrieve them for later use',
+        background_color: '#225ca3',
+        theme_color: '#225ca3',
+        start_url: '/',
+        publicPath: '/',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+         
+        ],
+      }),
     ],
 
     module: {
+      // add css loader
       rules: [
-        
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: 'asset/resource',
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            // add babel loader
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+            },
+          },
+        },
       ],
     },
   };
